b3e04f34918c64b72275f519c83c6d8e39a04051
Caroline Cho <caro@Carolines-MBP.attlocal.net>
2020-09-20 11:56:27 -0700

    Updates

---
+++ precache-manifest.a65ad5e46d9211fa1ff0147a534f8584.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc147b46db37523e0de726cacfb39649",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9d353bfced8dd642063",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0fa756a477c67805ea80",
+    "url": "/static/js/2.1850d562.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1850d562.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9d353bfced8dd642063",
+    "url": "/static/js/main.7dd7ad54.chunk.js"
+  },
+  {
+    "revision": "f5a32a3ef439df8f4927",
+    "url": "/static/js/runtime-main.6fd7a5a3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);

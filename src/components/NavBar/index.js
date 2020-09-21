8b188573d2af13f80e289a5a8e862f360a1b0c95
Caroline Cho <csc611@nyu.edu>
2020-09-20 21:50:07 -0700

    updating favicon and css

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -5,15 +5,15 @@ import './styles.css';
 function NavBar() {
   return (
     <nav id="nav-bar-link-wrapper">
-      <Link to="/">Home</Link>
-      <Link to="/week-1">Week 1</Link>
-      <Link to="/week-2">Week 2</Link>
-      <Link to="/week-3">Week 3</Link>
-      <Link to="/week-4">Week 4</Link>
-      <Link to="/week-5">Week 5</Link>
-      <Link to="/week-6">Week 6</Link>
-      <Link to="/week-7">Week 7</Link>
-      <Link to="/week-8">Week 8</Link>
+      <Link to="/" className={"link"}>Home</Link>
+      <Link to="/week-1" className={"link"}>Week 1</Link>
+      <Link to="/week-2" className={"link"}>Week 2</Link>
+      <Link to="/week-3" className={"link"}>Week 3</Link>
+      <Link to="/week-4" className={"link"}>Week 4</Link>
+      <Link to="/week-5" className={"link"}>Week 5</Link>
+      <Link to="/week-6" className={"link"}>Week 6</Link>
+      <Link to="/week-7" className={"link"}>Week 7</Link>
+      <Link to="/week-8" className={"link"}>Week 8</Link>
     </nav>
   );
 };
